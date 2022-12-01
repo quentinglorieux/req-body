189b47a0fe31aa81e9260dabb6f91eed3c468a52
Quentin Glorieux <quentin.glorieux@gmail.com>
2022-12-02 00:49:32 +0100

    res

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
     const { body } = req;
+    res.send('Test Page')
     return res.send(`Hello ${body.name}, you just parsed the request body!`);
   }
   
