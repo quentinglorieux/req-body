bcf1ed4bae781430f61baed0a9e85d2dd083116a
Quentin Glorieux <quentin.glorieux@gmail.com>
2022-12-02 00:50:45 +0100

    remove

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     const { body } = req;
-    res.send('Test Page')
+    //res.send('Test Page')
     return res.send(`Hello ${body.name}, you just parsed the request body!`);
   }
   
